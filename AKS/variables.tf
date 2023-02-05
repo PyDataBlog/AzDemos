8c02bbb5c23b1bdbd8eeeef9f28d9daafb5b5366
PyDataBlog <pimpfada@gmail.com>
2023-02-05 13:31:14 +0100

    Added admin to acr

--- AKS/variables.tf
+++ AKS/variables.tf
@@ -2,15 +2,6 @@ variable "agent_count" {
   default = 1
 }
 
-# The following two variable declarations are placeholder references.
-# # Set the values for these variable in terraform.tfvars
-# variable "aks_service_principal_app_id" {
-#   default = ""
-# }
-
-# variable "aks_service_principal_client_secret" {
-#   default = ""
-# }
 
 variable "cluster_name" {
   default = "k8stest"
